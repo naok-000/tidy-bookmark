8c4c11da6b1970c04a4fd3c24fa8662ffa8e47b8
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-03-20 17:55:10 +0900

    build: Initialize Nix flake and direnv configuration
    build: Refactor flake to use flake-utils and add Go project structure
    
    build: Add gotools and golangci-lint to nativeBuildInputs

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Tidy Bookmark";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nativeBuildInputs = with pkgs; [
+          go
+          gopls
+          gotools
+          golangci-lint
+        ];
+        buildInputs = with pkgs; [];
+      in {
+        devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
+
+        packages.default = pkgs.buildGoModule rec {
+          name = "tidy-bookmark";
+          src = ./.;
+
+          inherit buildInputs;
+
+          vendorHash = null;
+        };
+      }
+    );
+}
